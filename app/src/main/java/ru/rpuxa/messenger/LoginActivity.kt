761d6909d1d4ce346be634f9cb2286d2873934dc
Григорий <gr0070@mail.ru>
2019-09-08 21:51:15 +0400

    Some UI improvements

--- app/src/main/java/ru/rpuxa/messenger/LoginActivity.kt
+++ app/src/main/java/ru/rpuxa/messenger/LoginActivity.kt
@@ -2,11 +2,19 @@ package ru.rpuxa.messenger
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        content.alpha = 0f
+
+        content.animate()
+            .setDuration(1000)
+            .alpha(1f)
+            .start()
     }
 }
